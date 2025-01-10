9645f95b1ceb1797082a816f07ac5568a85444c5
sairaghuram <raghuram@acceldata.io>
2025-01-10 13:07:29 +0530

    @RespnseStatus and Exception Handling for specific HttpStatus

--- src/main/java/learn/springframwork/spring6restmvc/controller/ExceptionController.java
+++ src/main/java/learn/springframwork/spring6restmvc/controller/ExceptionController.java
@@ -5,7 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-// Global Exception Handler for all Controller defined
+/* Global Exception Handler for all Controller defined */
+/* Redundant now as we are using @ResponseStatus in ExceptionClass(Handling all the controller at one place) */
 @ControllerAdvice
 public class ExceptionController {
 
